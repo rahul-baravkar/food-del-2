7f6b65c1b852e96079d52e95a281659fe13b2062
rahulbaravkar-new-account <baravkarrahul400@gmail.com>
2026-04-10 23:47:52 +0530

    changes

--- admin/src/context/AdminContext.jsx
+++ admin/src/context/AdminContext.jsx
@@ -4,8 +4,8 @@ import { createContext } from "react";
 export const AdminContext = createContext(null);
 
 const AdminContextProvider = (props) => {
-  // const url = "https://food-del-2-backend-znom.onrender.com";
-  const url = "http://localhost:4000";
+  const url = "https://food-del-2-backend-znom.onrender.com";
+  // const url = "http://localhost:4000";
 
   const [token, setToken] = useState(localStorage.getItem("adminToken"));
 

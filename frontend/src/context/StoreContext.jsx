6ebe422de229a1683fa53365eff352ab893fceba
rahulbaravkar-new-account <baravkarrahul400@gmail.com>
2026-04-10 17:16:34 +0530

    add backend url

--- frontend/src/context/StoreContext.jsx
+++ frontend/src/context/StoreContext.jsx
@@ -6,7 +6,7 @@ export const StoreContext = createContext(null);
 const StoreContextProvider = (props) => {
   // const url = "https://food-del-2-backend-znom.onrender.com";
 
-  const url = "http://localhost:4000";
+  const url = "https://food-del-2-backend-znom.onrender.com";
 
   // ✔ FIX: token should NOT be " "
   const [token, setToken] = useState("");
